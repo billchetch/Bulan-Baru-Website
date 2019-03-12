30600229cac4196cfffb78f232e63c7a7fec0e42
billchetch <bill@chetch.net>
2019-03-12 23:08:03 +0800

    Test

--- scripts/test.php
+++ scripts/test.php
@@ -6,9 +6,21 @@ $log->start("Test script");
 
 
 try{
-	$email = 'xxx'; //@x.com';
-	Validate::email($email);
+	$mailer = 'PHPMailer_v5.0.2';
+	require("$phplib/$mailer/class.phpmailer.php");
+	require($root.'/_funcs.php');
+
+	$mail = new PHPMailer(true);
+	$mail->SetLanguage('en', "$phplib/$mailer/language/");
+	$mail->IsHTML(true);
+	$mail->CharSet = 'UTF-8';
 	
+	$mail->Subject = "test";
+	$mail->Body = "test";
+	$mail->From = 'info@bulan-baru.com';
+	$mail->FromName = 'BB';
+	
+	echo $mail->Send();
 		
 } catch (Exception $e){
 	$log->logException($e->getMessage());
